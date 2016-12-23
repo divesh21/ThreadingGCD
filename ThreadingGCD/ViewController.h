9eb0905d8efe2eaa42ce4719eba1c493d952aacc
student14 <student14@apples-macbook-air-4.local>
2016-12-23 11:47:22 +0530

    Initial Commit

---
+++ ThreadingGCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThreadingGCD
+//
+//  Created by student14 on 23/12/16.
+//  Copyright © 2016 Felix-ITs-Divesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ThreadingGCDTests/ThreadingGCDTests.m
@@ -0,0 +1,39 @@
+//
+//  ThreadingGCDTests.m
+//  ThreadingGCDTests
+//
+//  Created by student14 on 23/12/16.
+//  Copyright © 2016 Felix-ITs-Divesh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ThreadingGCDTests : XCTestCase
+
+@end
+
+@implementation ThreadingGCDTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
